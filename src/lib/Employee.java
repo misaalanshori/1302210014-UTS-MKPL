81342bc773791e0d622511789b5c5e3a7a2e9e6d
M Isa Al Anshori <isabungsu@gmail.com>
2024-04-23 16:20:45 +0700

    Menggantikan data spouse employee dengan class baru
    Membuat class Spouse dan menggantikan penggunaan String di Employee dengan class Spouse

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -25,8 +25,7 @@ public class Employee {
 	private int otherMonthlyIncome;
 	private int annualDeductible;
 	
-	private String spouseName;
-	private String spouseIdNumber;
+	private Spouse spouse;
 
 	private List<String> childNames;
 	private List<String> childIdNumbers;
@@ -79,9 +78,8 @@ public class Employee {
 		this.otherMonthlyIncome = income;
 	}
 	
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
+	public void setSpouse(Spouse spouse) {
+		this.spouse = spouse;
 	}
 	
 	public void addChild(String childName, String childIdNumber) {
@@ -100,6 +98,6 @@ public class Employee {
 			monthWorkingInYear = 12;
 		}
 		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouse.getIdNumber().equals(""), childIdNumbers.size());
 	}
 }
