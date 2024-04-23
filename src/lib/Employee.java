f234c2559e6c4fb967ed1eb4ed463b6aa3d07777
M Isa Al Anshori <isabungsu@gmail.com>
2024-04-23 17:52:27 +0700

    Menghapus komentar untuk setMonthlySalary
    Menghapus komentar yang tidak diperlukan untuk method Employee.setMonthlySalary yang sudah cukup jelas

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -43,11 +43,6 @@ public class Employee {
 		child = new LinkedList<RelatedPerson>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
