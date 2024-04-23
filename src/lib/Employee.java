e193c560a8876634b43fe3d87d654e2e8a7698b9
M Isa Al Anshori <isabungsu@gmail.com>
2024-04-23 16:41:38 +0700

    Menyederhanakan method setMonthlySalary
    Memindahkan check warga negara asing ke akhir method untuk menghilangkan pengulangan kode, serta memperbaiki bug dimana gaji warga asing selalu 3000000 * 1.5

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -52,19 +52,15 @@ public class Employee {
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
+		} else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
+		} else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+		} else {
+			return;
+		}
+		if (isForeigner) {
+			monthlySalary = (int) (monthlySalary * 1.5);
 		}
 	}
 	
